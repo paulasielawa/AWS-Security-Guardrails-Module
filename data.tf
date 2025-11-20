d2e0e8bbb9a547954079afd744f1ff3522d27c5a
paula.sielawa <paula.sielawa@soprasteria.com>
2025-11-20 15:36:29 +0100

    Add enabled cloudtrail, bucket, optionmal trail creation, multi-region supprt, cloudwatch logs integration, optional kms encryption, optional organization-wide trail. add new readme file. Fix arns of security hub standards

--- data.tf
+++ data.tf
@@ -3,3 +3,8 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
 
+data "aws_partition" "current" {}
+
+data "aws_organizations_organization" "current" {
+    count = var.enable_organization_trail ? 1 : 0
+}
